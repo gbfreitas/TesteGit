9a7a704d74605519c84094aeb16edf5c980b1f05
glauco <glaucobapfreitas@yahoo.com.br>
2018-03-27 08:03:20 -0300

    Adicionando projeto

---
+++ src/testegit/TesteGit.java
@@ -0,0 +1,15 @@
+
+package testegit;
+
+/**
+ *
+ * @author Glauco Freitas
+ */
+public class TesteGit {
+
+   
+    public static void main(String[] args) {
+       System.out.println("Testando Git..");
+    }
+    
+}
